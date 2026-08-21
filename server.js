eccd8fa5b97bf9ef98047f2da969ab8b4e764bea
mariamejez-svg <mariamejez@gmail.com>
2026-08-21 17:07:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,11 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
+console.log("CLOUDINARY CHECK:", {
+    cloud_name: !!process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: !!process.env.CLOUDINARY_API_KEY,
+    api_secret: !!process.env.CLOUDINARY_API_SECRET
+});
 /* =========================
    HOME
 ========================= */
